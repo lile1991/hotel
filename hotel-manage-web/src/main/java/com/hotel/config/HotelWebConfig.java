a143515753aae8122ebc55bcc9de52b0229e9579
lile <285051173@qq.com>
2017-12-08 15:37:16 +0800

    解决配置问题

--- hotel-manage-web/src/main/java/com/hotel/config/HotelWebConfig.java
+++ hotel-manage-web/src/main/java/com/hotel/config/HotelWebConfig.java
@@ -2,8 +2,31 @@ package com.hotel.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.hotel.controller")
-public class HotelWebConfig {
+public class HotelWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT, POST, GET, OPTIONS, DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Content-Type")
+                .allowCredentials(false).maxAge(3600);
+        super.addCorsMappings(registry);
+    }
+
+
 }
