37977cea0ad4b2285c95a850e0c5ca3ff83c9411
lile <285051173@qq.com>
2017-12-11 19:24:47 +0800

    配置好 Hibernate5Module到MessageConvert

--- hotel-manage-web/src/main/java/com/hotel/config/HotelWebConfig.java
+++ hotel-manage-web/src/main/java/com/hotel/config/HotelWebConfig.java
@@ -1,14 +1,18 @@
 package com.hotel.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -39,9 +43,22 @@ public class HotelWebConfig extends WebMvcConfigurerAdapter {
         objectMapper.registerModule(new Hibernate5Module());
         return objectMapper;
     }*/
-    
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter(objectMapper()));
+        super.configureMessageConverters(converters);
+    }
+
     @Bean
-    public Hibernate5Module hibernate5Module(SessionFactory sessionFactory) {
-        return new Hibernate5Module(sessionFactory);
+    public Hibernate5Module hibernate5Module() {
+        return new Hibernate5Module();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(hibernate5Module());
+        return objectMapper;
     }
 }
