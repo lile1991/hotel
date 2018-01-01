104c9d9bdb32f0d2f3fce004f280f0452150c337
lile <285051173@qq.com>
2018-01-01 14:37:23 +0800

    跨域cookie

--- hotel-manage-web/src/main/java/com/hotel/config/HotelWebConfig.java
+++ hotel-manage-web/src/main/java/com/hotel/config/HotelWebConfig.java
@@ -2,12 +2,15 @@ package com.hotel.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -24,7 +27,7 @@ public class HotelWebConfig extends WebMvcConfigurerAdapter {
         super.addInterceptors(registry);
     }
 
-    @Override
+    /*@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:9528")
@@ -34,15 +37,23 @@ public class HotelWebConfig extends WebMvcConfigurerAdapter {
                 .exposedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true).maxAge(3600);
         super.addCorsMappings(registry);
-    }
-
-    /*@Bean
-    @ConditionalOnBean(Hibernate5Module.class)
-    public ObjectMapper createObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new Hibernate5Module());
-        return objectMapper;
     }*/
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:9528");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
--- hotel-manage-web/src/main/java/com/hotel/config/ShiroConfig.java
+++ hotel-manage-web/src/main/java/com/hotel/config/ShiroConfig.java
@@ -33,15 +33,22 @@ public class ShiroConfig implements ApplicationContextAware {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         //Shiro的核心安全接口,这个属性是必须的
         shiroFilterFactoryBean.setSecurityManager(securityManager());
+
         //要求登录时的链接(可根据项目的URL进行替换),非必须的属性,默认会自动寻找Web工程根目录下的"/login.jsp"页面
 //        shiroFilterFactoryBean.setLoginUrl("/login");
+
         //登录成功后要跳转的连接,逻辑也可以自定义，例如返回上次请求的页面
 //        shiroFilterFactoryBean.setSuccessUrl("/index");
+
         //用户访问未对其授权的资源时,所显示的连接
         shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+
         /*定义shiro过滤器,例如实现自定义的FormAuthenticationFilter，需要继承FormAuthenticationFilter
          **本例中暂不自定义实现，在下一节实现验证码的例子中体现
          */
+//        Map<String, Filter> filters = new HashMap<>();
+//        filters.put("authc", authenticatingFilter());
+//        shiroFilterFactoryBean.setFilters(filters);
 
         /*定义shiro过滤链  Map结构
          * Map中key(xml中是指value值)的第一个'/'代表的路径是相对于HttpServletRequest.getContextPath()的值来的
