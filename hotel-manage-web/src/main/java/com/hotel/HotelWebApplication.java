00084ba6f2bb38e80661b3b500fe59191cee0477
lile <285051173@qq.com>
2018-01-27 17:24:54 +0800

    Fuck!

--- hotel-manage-web/src/main/java/com/hotel/HotelWebApplication.java
+++ hotel-manage-web/src/main/java/com/hotel/HotelWebApplication.java
@@ -1,10 +1,14 @@
 package com.hotel;
 
+import com.hotel.config.HotelWebConfiguration;
+import com.hotel.config.ShiroConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(exclude = {LiquibaseAutoConfiguration.class})
+@Import({HotelWebConfiguration.class, ShiroConfiguration.class})
 public class HotelWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelWebApplication.class);
